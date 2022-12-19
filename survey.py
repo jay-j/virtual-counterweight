a77bc43f6652c59ddbe3d2050b5c22bd48d55fce
jay-j <jay-j@github.com>
2022-12-19 09:59:08 -0600

    Go over both spring_qty 1 and 2 in a single survey, generate a common results file

--- survey.py
+++ survey.py
@@ -17,6 +17,7 @@ solution_datatype = np.dtype([
     ("residual", float),
     ("spring_qty", int),
     ("solve_success", int),
+    ("max_error_ratio", float),
 
     ("arm_length0", float),
     ("spring_origin_x0", float),
@@ -28,57 +29,56 @@ solution_datatype = np.dtype([
     ("spring_origin_y1", float),
     ("cable_length1", float),
 ])
+all_solutions = pd.DataFrame(np.zeros(2*len(catalog), dtype=solution_datatype))
 
-all_solutions = pd.DataFrame(np.zeros(len(catalog), dtype=solution_datatype))
 
+# actually do the survey
+for spring_qty in range(1,3):
+    print(f"Searching the catalog with spring_qty={spring_qty}...")
 
-best_value = 1e9
-best_index = -1
-best_soln = None
-
-# TODO go back to searching the entire thing
-for i in range(240, len(catalog)):
-    item = catalog.iloc[i]
+    # TODO go back to searching the entire thing
+    for spring_index in range(240, len(catalog)):
+        i = spring_index + (spring_qty-1)*len(catalog)
+        item = catalog.iloc[spring_index]
     
-    spring_qty = 1
-    test_spring = cw.Spring(stiffness=item["stiffness"], length_natural=item["length_natural"], length_max=item["length_max"])
-    soln = cw.spring_optimize(test_spring, spring_qty)
+        test_spring = cw.Spring(stiffness=item["stiffness"], length_natural=item["length_natural"], length_max=item["length_max"])
+        soln = cw.spring_optimize(test_spring, spring_qty)
     
-    print("i =", i)
-
-    # track the very best one
-    if soln.success and soln.fun < best_value:
-        print("New best.")#, i, soln)
-        best_value = soln.fun
-        best_index = i
-        best_soln = soln
+        if i % 10 == 0:
+            print(f"i = {i} of {len(catalog)}")
     
-    # save all the successes to a table
-    if soln.success:
-        print(soln)
-        all_solutions.loc[i, "solve_success"] = 1
-        all_solutions.loc[i, "spring_index"] = i
-        all_solutions.loc[i, "residual"] = soln.fun
-        all_solutions.loc[i, "spring_qty"] = spring_qty
+        # save all the successes to a table
+        if soln.success:
+            # get the percentage error value
+            vc_list = cw.vc_list_from_parameters(soln.x, test_spring, spring_qty)
+            net_torque = cw.compute_net_torque(vc_list)
+            max_error_ratio = np.max(np.abs(net_torque)) / cw.gravity_torque_max
+            
+            print("Success", max_error_ratio, soln.x)
+            all_solutions.loc[i, "solve_success"] = 1
+            all_solutions.loc[i, "spring_index"] = spring_index
+            all_solutions.loc[i, "residual"] = soln.fun
+            all_solutions.loc[i, "spring_qty"] = spring_qty
+            all_solutions.loc[i, "max_error_ratio"] = max_error_ratio
 
-        offset = 4
-        all_solutions.loc[i, "arm_length0"] = soln.x[0]
-        all_solutions.loc[i, "arm_length1"] = soln.x[0 + offset]
+            offset = 4
+            all_solutions.loc[i, "arm_length0"] = soln.x[0]
+            all_solutions.loc[i, "arm_length1"] = soln.x[0 + offset]
 
-        all_solutions.loc[i, "spring_origin_x0"] = soln.x[1]
-        all_solutions.loc[i, "spring_origin_x1"] = soln.x[1 + offset]
+            all_solutions.loc[i, "spring_origin_x0"] = soln.x[1]
+            all_solutions.loc[i, "spring_origin_x1"] = soln.x[1 + offset]
         
-        all_solutions.loc[i, "spring_origin_y0"] = soln.x[2]
-        all_solutions.loc[i, "spring_origin_y1"] = soln.x[2 + offset]
+            all_solutions.loc[i, "spring_origin_y0"] = soln.x[2]
+            all_solutions.loc[i, "spring_origin_y1"] = soln.x[2 + offset]
         
-        all_solutions.loc[i, "cable_length0"] = soln.x[3]
-        all_solutions.loc[i, "cable_length1"] = soln.x[3 + offset]
+            all_solutions.loc[i, "cable_length0"] = soln.x[3]
+            all_solutions.loc[i, "cable_length1"] = soln.x[3 + offset]
 
         
 # remove all spring solutions that did not successfully solve
 all_solutions = all_solutions[all_solutions.solve_success == 1]
 
-# TODO save out the database to an excel file for manual exploration of the solution space
+# save out the database to an excel file for manual exploration of the solution space
 all_solutions.to_csv("solutions.csv")
 
 print("complete")
