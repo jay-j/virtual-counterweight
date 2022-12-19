8491d9071cab785b002201574101e661c5e9141f
jay-j <jay-j@github.com>
2022-12-19 09:59:30 -0600

    Cleanup

--- check_one.py
+++ check_one.py
@@ -7,24 +7,21 @@ import simple_spring as cw
 
 catalog = pd.read_csv("springs_english.csv", sep=",")
 
-spring_qty = 2
 
+########
 # Choose a specific spring solution of interest. This should already be optimized.
+spring_qty = 2
 index = 1010 # index in the spring catalog
 soln = np.array([ 1.51075338e-02,  1.30000000e-01, -2.70000000e-01,  0.00000000e+00,
         1.50000000e-02, -8.60161725e-02, -2.70000000e-01,  3.28437002e-17])
 
-# double spring exploring
-index = 952
-soln = np.array([0.020, 0.130, -0.270, 0.000, 
-                0.019, -0.082, -0.270, 0.000])
-
 # single spring best
-# index = 835
-# soln = np.array([0.021, 0.032, -0.231, 0.000, 
-#                 0.015, 0.050, -0.200, 0.000])
-# spring_qty = 1
+spring_qty = 1
+index = 921
+soln = np.array([0.015, 0.024, -0.265, 0.000, 
+                0.015, 0.050, -0.200, 0.000])
 
+## Now actually run the calculation
 item = catalog.iloc[index]
 test_spring = cw.Spring(stiffness=item["stiffness"], length_natural=item["length_natural"], length_max=item["length_max"])
 
